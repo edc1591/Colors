a15331ce9dff042ed99d01c04bdaf4c1757d9892
Evan Coleman <e@edc.me>
2015-11-28 11:31:24 -0500

    Initial Commit

---
+++ Colors/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Colors
+//
+//  Created by Evan Coleman on 11/28/15.
+//  Copyright © 2015 Evan Coleman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
